65683f75209abd685c298fd2bcf73b79b621178b
TV <trungvuh@gmail.com>
2017-12-03 19:51:07 -0800

    Working on mouse movement

--- bundle.js
+++ bundle.js
@@ -68,21 +68,46 @@
 /***/ (function(module, exports) {
 
 document.addEventListener("DOMContentLoaded", function(){
-  const canvasEl = document.getElementById("myCanvas");
-  canvasEl.width = 1000;
-  canvasEl.height = 1000;
+  var canvas = document.getElementById("myCanvas");
+  canvas.width = 480;
+  canvas.height = 640;
 
-  const ctx = canvasEl.getContext("2d");
-  ctx.fillStyle = "teal";
-  ctx.fillRect(200, 0, 480, 640);
+  var ctx = canvas.getContext("2d");
+  var ballCenterX = (canvas.width - 20);
+  var ballCenterY = (canvas.height - 20);
 
-  ctx.beginPath();
-  ctx.arc(300, 300, 20, 0, 2*Math.PI, true);
-  ctx.strokeStyle = "red";
-  ctx.lineWidth = 5;
-  ctx.stroke();
-  ctx.fillStyle = "yellow";
-  ctx.fill();
+  function drawBall() {
+    ctx.beginPath();
+    ctx.arc(ballCenterX, ballCenterY, 20, 0, 2*Math.PI, true);
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 5;
+    ctx.stroke();
+    ctx.fillStyle = "yellow";
+    ctx.fill();
+    ctx.closePath();
+  }
+
+
+  document.addEventListener("mousemove", mouseMoveHandler, false);
+
+  function mouseMoveHandler(e) {
+      var relativeX = e.clientX;
+      if(relativeX > 0 && relativeX < canvas.width) {
+          ballCenterX = relativeX - 10;
+      }
+      var relativeY = e.clientY;
+      if(relativeY > 0 && relativeY < canvas.height) {
+          ballCenterY = relativeY - 10;
+      }
+  }
+
+  function draw() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      drawBall();
+  }
+
+  setInterval(draw, 10);
+
 });
 
 
